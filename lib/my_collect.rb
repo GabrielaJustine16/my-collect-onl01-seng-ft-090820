21fdeebbf80fc1874b20f241c763e6c8c2fb655a
Gabriela johnson <johnsongabriela12@gmail.com>
2021-05-13 23:39:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
-my_collect(collection) do |language|
-  i = 0 
-  new_collect = 0 
-  while i < language.length 
-  new_collect << yield(collection[i])
-end 
 
-new_collect
-  
-end 
+  def my_collect(collection)
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
